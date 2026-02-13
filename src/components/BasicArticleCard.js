1fe3ddf6a4ad849f3115220361850e2d2a607ec1
Samson Sanyaolu <sosarenaint@gmail.com>
2026-02-13 21:30:37 +0400

    Refactor BasicArticleCard for cleaner syntax

--- src/components/BasicArticleCard.js
+++ src/components/BasicArticleCard.js
@@ -5,68 +5,45 @@ import Tag from "./Tag";
 
 import "./basicArticleCard.css";
 
-const BasicArticleCard = (props) => {
-  let article = props.data;
+const BasicArticleCard = ({ data: article }) => {
+  const handleScrollTop = () => scroll.scrollToTop();
+
   return (
     <div className="basic-article-card__container">
       <div className="basic-article-card__container__image-div">
-        <Link
-          to={{
-            pathname: `/articles/${article.slug}`,
-            state: article.articleId,
-          }}
-          onClick={() => scroll.scrollToTop()}
-        >
+        <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
           <img src={article.img} alt={article.title} />
         </Link>
       </div>
+
       <div className="basic-article-card__container__content-div">
         <Tag tag={article.tag[0]} />
+
         <div className="basic-article-card__container__content-div__title">
           <h2>
-            <Link
-              to={{
-                pathname: `/articles/${article.slug}`,
-                state: article.articleId,
-              }}
-              onClick={() => scroll.scrollToTop()}
-            >
+            <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
               {article.title}
             </Link>
           </h2>
         </div>
+
         <div className="basic-article-card__container__content-div__para">
           <p>{article.desc}</p>
         </div>
+
         <div className="basic-article-card__container__content-div__end">
           <div className="basic-article-card__container__content-div__end-readMore">
             <p>
-              <Link
-                to={{
-                  pathname: `/articles/${article.slug}`,
-                  state: article.articleId,
-                }}
-                onClick={() => scroll.scrollToTop()}
-              >
+              <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
                 Read More
               </Link>
             </p>
           </div>
+
           <div className="basic-article-card__container__content-div__end-authors">
             <div className="basic-article-card__container__content-div__end-authors__circle">
-              <Link
-                to={{
-                  pathname: `/authors`,
-                  state: {
-                    authorImg: article.author,
-                    authorName: article.authorName,
-                  },
-                }}
-              >
-                <img
-                  src={article.author}
-                  alt={article.title + article.tag[0]}
-                />
+              <Link to="/authors">
+                <img src={article.author} alt={`${article.title} - ${article.tag[0]}`} />
               </Link>
             </div>
           </div>
