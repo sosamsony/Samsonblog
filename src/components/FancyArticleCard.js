3ed8648626e8131a11051443587d2aab7326d2d5
Samson Sanyaolu <sosarenaint@gmail.com>
2026-02-13 21:30:30 +0400

    Refactor FancyArticleCard to use destructuring and links

--- src/components/FancyArticleCard.js
+++ src/components/FancyArticleCard.js
@@ -5,58 +5,41 @@ import Tag from "./Tag";
 
 import "./fancyArticleCard.css";
 
-const FancyArticleCard = (props) => {
-  let article = props.data;
+const FancyArticleCard = ({ data: article }) => {
+  const handleScrollTop = () => scroll.scrollToTop();
 
   return (
     <div className="fancy-article-card__container">
       <div className="fancy-article-card__container__image-div">
-        <img src={article.img} alt={article.title} />
+        <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
+          <img src={article.img} alt={article.title} />
+        </Link>
       </div>
+
       <div className="fancy-article-card__container__content-div">
         <Tag tag={article.tag[0]} />
+
         <div className="basic-article-card__container__content-div__title">
           <h2>
-            <Link
-              to={{
-                pathname: `/articles/${article.slug}`,
-                state: article.articleId,
-              }}
-              onClick={() => scroll.scrollToTop()}
-            >
+            <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
               {article.title}
             </Link>
           </h2>
         </div>
+
         <div className="basic-article-card__container__content-div__end">
           <div className="basic-article-card__container__content-div__end-readMore">
             <p>
-              <Link
-                to={{
-                  pathname: `/articles/${article.slug}`,
-                  state: article.articleId,
-                }}
-                onClick={() => scroll.scrollToTop()}
-              >
+              <Link to={`/articles/${article.slug}`} onClick={handleScrollTop}>
                 Read More
               </Link>
             </p>
           </div>
+
           <div className="basic-article-card__container__content-div__end-authors">
             <div className="basic-article-card__container__content-div__end-authors__circle">
-              <Link
-                to={{
-                  pathname: `/authors`,
-                  state: {
-                    authorImg: article.author,
-                    authorName: article.authorName,
-                  },
-                }}
-              >
-                <img
-                  src={article.author}
-                  alt={article.title + article.tag[0]}
-                />
+              <Link to="/authors">
+                <img src={article.author} alt={`${article.title} - ${article.tag[0]}`} />
               </Link>
             </div>
           </div>
